7f8aeb23b9b9dc3644e6b7277186c8c47f747017
PMJ <pmj@pmj.hu>
2020-12-27 12:39:12 +0100

    Allow other subcommands to be called

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,5 +11,13 @@ if [ $CONTENT_LENGTH == "0" ] || [ "$CONTENT" == ".git" ]; then
     printf 'y\n \n' | mdbook init --force --theme
 fi
 
-# START SERVING BOOK
-mdbook serve --hostname 0.0.0.0 --port 3000
+# IF NO FIRST ARG SUPPLIED OR FIRST ARG IS "serve"
+if test -z $1 || [ "$1" == "serve" ]; then
+
+    # START SERVING BOOK
+    mdbook serve --hostname 0.0.0.0 --port 3000
+    exit $?
+fi
+
+# IF PARAMS SUPPLIED, PASS THEM TO MAIN COMMAND
+mdbook $@
